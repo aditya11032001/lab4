18e8ccbdc939236da66d30123f3dc0a9ef7aaffd
Aditya <Aditya@DESKTOP-5J8IC55>
2023-08-11 09:16:00 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] mat = new int[3, 3];
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    mat[i, j] = i + j;//Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                    Console.Write(mat[i,j] + " ");
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
